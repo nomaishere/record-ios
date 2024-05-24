504f5f02ce94258fc78ff9c04950979d8cd91bb4
Sangmin Kim <noma@intereal.io>
2024-05-24 14:41:31 +0900

    add: fonts (Pretendard, Profont for Powerline, Poppins)

---
+++ Record/Views/Collection.swift
@@ -0,0 +1,18 @@
+//
+//  Collection.swift
+//  Record
+//
+//  Created by nomamac2 on 5/24/24.
+//
+
+import SwiftUI
+
+struct Collection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Collection()
+}
