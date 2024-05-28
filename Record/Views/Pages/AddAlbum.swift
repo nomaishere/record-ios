8bf958dc2fde6b00f9c4fab4bb4af39504392005
Sangmin Kim <noma@intereal.io>
2024-05-28 19:34:07 +0900

    commit: extract border extension

--- Record/Views/Pages/AddAlbum.swift
+++ Record/Views/Pages/AddAlbum.swift
@@ -7,33 +7,6 @@
 
 import SwiftUI
 
-extension View {
-    func border(width: CGFloat, edges: [Edge], color: Color) -> some View {
-        overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
-    }
-    
-    func strokeBorder(lineWidth: CGFloat, edges: [Edge], color: Color) -> some View {
-        overlay(EdgeBorder(width: lineWidth, edges: edges).foregroundColor(color))
-    }
-}
-
-
-struct EdgeBorder: Shape {
-    var width: CGFloat
-    var edges: [Edge]
-
-    func path(in rect: CGRect) -> Path {
-        edges.map { edge -> Path in
-            switch edge {
-            case .top: return Path(.init(x: rect.minX, y: rect.minY, width: rect.width, height: width))
-            case .bottom: return Path(.init(x: rect.minX, y: rect.maxY - width, width: rect.width, height: width))
-            case .leading: return Path(.init(x: rect.minX, y: rect.minY, width: width, height: rect.height))
-            case .trailing: return Path(.init(x: rect.maxX - width, y: rect.minY, width: width, height: rect.height))
-            }
-        }.reduce(into: Path()) { $0.addPath($1) }
-    }
-}
-
 struct AddAlbum: View {
     
     @EnvironmentObject var router: Router
@@ -47,9 +20,9 @@ struct AddAlbum: View {
         .fill(Color("G1"))
         .strokeBorder(Color("G2") , lineWidth: 2, antialiased: true)
         .border(width: 2, edges: [.top], color: Color("G1"))
-        .frame(height: 400, alignment: .top)
+        .frame(height: 188, alignment: .top)
         .overlay(
-            VStack {
+            VStack(spacing: 0) {
                 ZStack {
                     HStack {
                         Button() {
@@ -71,21 +44,8 @@ struct AddAlbum: View {
                         .foregroundStyle(Color("G7"))
                 }
                 .padding(.bottom, 24)
-                
-                HStack {
-                    Text("Import")
-                    Spacer()
-                    Text("Tracklist")
-                    Spacer()
-                    Text("Metadata")
-                    Spacer()
-                    Text("Check")
-                }
-                .font(Font.custom("Poppins-Medium", size: 16))
-                .foregroundStyle(Color("G3"))
-                .padding(.horizontal, 24)
-                Divider()
                 ProgressWithText()
+                    .padding(.bottom, 24)
                 
             }
                 .padding(.top, 60)
