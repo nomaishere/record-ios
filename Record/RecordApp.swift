1ae6b52e5b40ef78c1f1be8619830ab0daf53223
Sangmin Kim <noma@intereal.io>
2024-06-20 11:27:03 +0900

    add: safeAreaInset getter

--- Record/RecordApp.swift
+++ Record/RecordApp.swift
@@ -8,42 +8,76 @@
 import SwiftData
 import SwiftUI
 
+// TODO: Cleanup Code
+extension UIApplication {
+    var keyWindow: UIWindow? {
+        connectedScenes
+            .compactMap {
+                $0 as? UIWindowScene
+            }
+            .flatMap {
+                $0.windows
+            }
+            .first {
+                $0.isKeyWindow
+            }
+    }
+}
+
+private struct SafeAreaInsetsKey: EnvironmentKey {
+    static var defaultValue: EdgeInsets {
+        UIApplication.shared.keyWindow?.safeAreaInsets.swiftUiInsets ?? EdgeInsets()
+    }
+}
+
+extension EnvironmentValues {
+    var safeAreaInsets: EdgeInsets {
+        self[SafeAreaInsetsKey.self]
+    }
+}
+
+private extension UIEdgeInsets {
+    var swiftUiInsets: EdgeInsets {
+        EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
+    }
+}
+
 @main
 struct RecordApp: App {
     @ObservedObject var router = Router()
-    
+
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Album.self, Artist.self, Track.self, Genre.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-        
+
         do {
             let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
-            
+
             // Check application has genre data
             let descriptor = FetchDescriptor<Genre>()
             let existingGenres = try container.mainContext.fetchCount(descriptor)
             guard existingGenres == 0 else { return container }
-            
+
             // Load built-in genre data & decode
             guard let url = Bundle.main.url(forResource: "BuiltInGenreData", withExtension: "json") else {
                 fatalError("Failed to find users.json")
             }
-            
+
             let data = try Data(contentsOf: url)
             let genreDatas = try JSONDecoder().decode([Genre].self, from: data)
-            
+
             for genreData in genreDatas {
                 container.mainContext.insert(genreData)
             }
-            
+
             return container
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
-    
+
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: $router.navPath) {
