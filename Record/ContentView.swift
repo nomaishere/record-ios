4329e7b1752121c10015c827f842e48f4c2592ca
Sangmin Kim <noma@intereal.io>
2024-06-21 12:52:21 +0900

    refactor: delete PlayerViewModel

--- Record/ContentView.swift
+++ Record/ContentView.swift
@@ -13,19 +13,16 @@ struct ContentView: View {
 
     @State var selectedTab: Tab = .Collection
 
-    @StateObject var viewModel = PlayerViewModel()
+    @State var isPlayerMinimized: Bool = true
 
     var body: some View {
         ZStack(alignment: .bottom) {
             DomainView(selectedTab: $selectedTab)
                 .padding(.top, safeAreaInsets.top)
-            PlayerView()
-                .environmentObject(viewModel)
-                .zIndex(viewModel.playerMode == .minibar ? 0 : 1) // increase this
+            PlayerView(isPlayerMinimized: $isPlayerMinimized, bottomPadding: safeAreaInsets.bottom + 59 + 8)
+                .zIndex(isPlayerMinimized ? 0 : 1) // increase this
 
             NavigationBar(selectedTab: $selectedTab)
-
-            // .ignoresSafeArea()
         }
         .ignoresSafeArea()
     }
@@ -54,15 +51,81 @@ struct DomainView: View {
 }
 
 struct PlayerView: View {
-    /*
-     @State private var isDragging: Bool = false
-     @State private var doOnceLock: Bool = false
-     @State private var testHeight: CGFloat = 64
-     @State private var testWidth: CGFloat = UIScreen.main.bounds.size.width - 16
-     @State private var bottomPadding: CGFloat = 67
-     */
+    enum PlayerMode {
+        case minibar
+        case expanded
+        case animated
+    }
 
-    @EnvironmentObject var viewModel: PlayerViewModel
+    @Binding var isPlayerMinimized: Bool
+    @Environment(\.safeAreaInsets) private var safeAreaInsets
+
+    @State var playerMode: PlayerMode = .minibar
+
+    @State var viewHeight: CGFloat = 64
+    @State var viewWidth: CGFloat = UIScreen.main.bounds.size.width - 16
+    @State var bottomPadding: CGFloat
+
+    @State var isMinibarItemRender: Bool = true
+
+    @State var isDragging: Bool = false
+    @State var doOnceLock: Bool = false
+
+    let minibarBottomPadding: CGFloat
+
+    init(isPlayerMinimized: Binding<Bool>, bottomPadding: CGFloat) {
+        _isPlayerMinimized = isPlayerMinimized
+        _bottomPadding = State(initialValue: bottomPadding)
+        minibarBottomPadding = bottomPadding
+    }
+
+    var dragGesture: some Gesture {
+        DragGesture(minimumDistance: 10)
+            .onChanged { value in
+                self.isDragging = true
+
+                // MARK: - Expand Player (Swipe Up)
+
+                if value.translation.height < -30 {
+                    if !self.doOnceLock {
+                        self.doOnceLock.toggle()
+                        self.playerMode = .animated
+                        isPlayerMinimized = false
+                        withAnimation(.easeOut(duration: 0.15)) {
+                            self.viewWidth = UIScreen.main.bounds.size.width
+                            self.viewHeight = UIScreen.main.bounds.size.height
+                            self.bottomPadding = 0
+                            self.isMinibarItemRender.toggle() // Erase Minibar Item
+                        } completion: {
+                            self.playerMode = .expanded
+                            self.doOnceLock.toggle()
+                            self.isMinibarItemRender = false
+                        }
+                    }
+
+                    // MARK: - Minimize Player (Swipe Down)
+
+                } else if value.translation.height > 30 {
+                    if !self.doOnceLock {
+                        self.doOnceLock.toggle()
+                        self.playerMode = .animated
+
+                        withAnimation(.easeOut(duration: 0.15)) {
+                            self.viewWidth = UIScreen.main.bounds.size.width - 16
+                            self.viewHeight = 64
+                            self.bottomPadding = minibarBottomPadding
+
+                        } completion: {
+                            self.playerMode = .minibar
+                            self.doOnceLock.toggle()
+                            self.isMinibarItemRender = true
+                            isPlayerMinimized = true
+                        }
+                    }
+                }
+            }
+            .onEnded { _ in self.isDragging = false }
+    }
 
     var body: some View {
         ZStack {
@@ -71,9 +134,9 @@ struct PlayerView: View {
                     Image("ugrshigh")
                         .resizable()
                         .scaledToFill())
-            switch viewModel.playerMode {
+            switch playerMode {
             case .minibar:
-                if viewModel.isMinibarItemRender {
+                if isMinibarItemRender {
                     HStack {
                         Text("Somozu Combat")
                             .font(Font.custom("Poppins-SemiBold", size: 20))
@@ -94,10 +157,10 @@ struct PlayerView: View {
                 Text("ss")
             }
         }
-        .frame(width: viewModel.viewWidth, height: viewModel.viewHeight)
+        .frame(width: viewWidth, height: viewHeight)
         // .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
         .clipShape(RoundedRectangle(cornerRadius: 4))
-        .padding(.bottom, viewModel.bottomPadding)
-        .gesture(viewModel.dragGesture)
+        .padding(.bottom, bottomPadding)
+        .gesture(dragGesture)
     }
 }
