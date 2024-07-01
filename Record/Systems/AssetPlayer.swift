f22035eaef818eebc418c65e5d03261b56edd4c9
Sangmin Kim <noma@intereal.io>
2024-07-01 23:28:10 +0900

    add: bunch of codes for testing nowplayable support

--- Record/Systems/AssetPlayer.swift
+++ Record/Systems/AssetPlayer.swift
@@ -5,4 +5,35 @@
 //  Created by nomamac2 on 6/28/24.
 //
 
-import Foundation
+import AVFoundation
+import MediaPlayer
+
+/*
+ class AssetPlayer {
+ enum PlayerState {
+ case stopped
+ case playing
+ case paused
+ }
+ 
+ let player: AVQueuePlayer
+ private let playerItems: [AVPlayerItem]
+ private let staticMetadatas: [NowPlayableStaticMetadata]
+ 
+ private var playerState: PlayerState = .stopped {
+ didSet {
+ NSLog("%@", "**** Set player state \(playerState)")
+ }
+ }
+ 
+ // `true` if the current session has been interrupted by another app.
+ private var isInterrupted: Bool = false
+ 
+ // Private observers of notifications and property changes.
+ private var itemObserver: NSKeyValueObservation!
+ private var rateObserver: NSKeyValueObservation!
+ private var statusObserver: NSObjectProtocol!
+ 
+ init() throws {}
+ }
+ */
--- Record/Systems/NowPlayable/NowPlayableMetadata.swift
+++ Record/Systems/NowPlayable/NowPlayableMetadata.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 import MediaPlayer
+
+struct NowPlayableStaticMetadata {
+    let assetURL: URL // MPNowPlayingInfoPropertyAssetURL
+    let mediaType: MPNowPlayingInfoMediaType // MPNowPlayingInfoPropertyMediaType
+    let isLiveStream: Bool // MPNowPlayingInfoPropertyIsLiveStream
+
+    let title: String // MPMediaItemPropertyTitle
+    let artist: String? // MPMediaItemPropertyArtist
+    let artwork: MPMediaItemArtwork? // MPMediaItemPropertyArtwork
+
+    let albumArtist: String? // MPMediaItemPropertyAlbumArtist
+    let albumTitle: String? // MPMediaItemPropertyAlbumTitle
+}
+
+struct NowPlayableDynamicMetadata {
+    let rate: Float // MPNowPlayingInfoPropertyPlaybackRate
+    let position: Float // MPNowPlayingInfoPropertyElapsedPlaybackTime
+    let duration: Float // MPMediaItemPropertyPlaybackDuration
+
+    let currentLanguageOptions: [MPNowPlayingInfoLanguageOption] // MPNowPlayingInfoPropertyCurrentLanguageOptions
+    let availableLanguageOptionGroups: [MPNowPlayingInfoLanguageOptionGroup] // MPNowPlayingInfoPropertyAvailableLanguageOptions
+}
