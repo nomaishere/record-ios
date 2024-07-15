4cc6ca22269f227f0cacf112139056088e9e2241
Sangmin Kim <noma@intereal.io>
2024-07-15 21:51:27 +0900

    add: color extension to support string type initialization

--- Record/Views/lib/ColorHexExtension.swift
+++ Record/Views/lib/ColorHexExtension.swift
@@ -18,4 +18,17 @@ extension Color {
             opacity: opacity
         )
     }
+
+    init(hexString: String, opacity: Double = 1) {
+        var rgb: UInt64 = 0xffffff
+        Scanner(string: hexString).scanHexInt64(&rgb)
+
+        self.init(
+            .sRGB,
+            red: Double((rgb >> 16) & 0xff) / 255,
+            green: Double((rgb >> 08) & 0xff) / 255,
+            blue: Double((rgb >> 00) & 0xff) / 255,
+            opacity: opacity
+        )
+    }
 }
