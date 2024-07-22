6543547ca24cfc8fd4ec682cc248e288426e424d
Sangmin Kim <noma@intereal.io>
2024-07-22 14:12:29 +0900

    refactor: extract view & delete unused things

--- Record/Views/Pages/Collection.swift
+++ Record/Views/Pages/Collection.swift
@@ -112,53 +112,29 @@ struct Collection: View {
                 }
             }
         } else {
-            VStack(alignment: .center) {
-                Spacer()
-                    .frame(height: 24)
-                HStack(alignment: .center) {
-                    Text("COLLECTION")
-                        .font(Font.custom("ProFont For Powerline", size: 32))
-                        .foregroundStyle(Color("DefaultBlack"))
+            ScrollView {
+                VStack {
                     Spacer()
-                    Button("Add", action: { router.navigate(to: .addalbum) })
-                        .padding(.vertical, 4.0)
-                        .padding(.horizontal, 24)
-                        .background(Color("DefaultBlack"))
-                        .font(Font.custom("Poppins-Medium", size: 20)).foregroundStyle(.white)
-                        .clipShape(Capsule())
-                }
-                .padding(.horizontal, 24.0)
-                Spacer()
-                    .frame(height: 24)
-                HStack {
-                    ZStack(alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 4)
-                            .fill(Color("G1"))
-                            .frame(height: 48)
-
-                        Text("Search")
-                            .font(Font.custom("Poppins-Medium", size: 18))
-                            .multilineTextAlignment(.leading)
-                            .foregroundStyle(Color("G3"))
-                            .padding(.leading, 16.0)
+                        .frame(height: 24)
+                    HStack(alignment: .center) {
+                        Text("COLLECTION")
+                            .font(Font.custom("ProFont For Powerline", size: 32))
+                            .foregroundStyle(Color("DefaultBlack"))
+                        Spacer()
+                        Button("Add", action: { router.navigate(to: .addalbum) })
+                            .padding(.vertical, 4.0)
+                            .padding(.horizontal, 24)
+                            .background(Color("DefaultBlack"))
+                            .font(Font.custom("Poppins-Medium", size: 20)).foregroundStyle(.white)
+                            .clipShape(Capsule())
                     }
+                    .padding(.horizontal, 24.0)
                     Spacer()
-                        .frame(width: 18)
-                    Button {
-                        print("order")
-                    } label: {
-                        Image("Order")
-                            .frame(width: 48, height: 48)
-                            .foregroundColor(Color("G3"))
-                            .background(Color("G1"))
-                            .clipShape(RoundedRectangle(cornerRadius: 4))
-                    }
-                }
-                .padding(.horizontal, 16.0)
-                Spacer()
-                    .frame(height: 16)
+                        .frame(height: 24)
+                    // SearchAndOrderView()
+                    Spacer()
+                        .frame(height: 16)
 
-                ScrollView {
                     AlbumGridList(albums: albums, onTabGestureHander: handleAlbumOnTabGesture)
                 }
             }
@@ -168,4 +144,35 @@ struct Collection: View {
 
 #Preview {
     Collection()
+}
+
+/// Deprecated View
+struct SearchAndOrderView: View {
+    var body: some View {
+        HStack {
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(Color("G1"))
+                    .frame(height: 48)
+
+                Text("Search")
+                    .font(Font.custom("Poppins-Medium", size: 18))
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(Color("G3"))
+                    .padding(.leading, 16.0)
+            }
+            Spacer()
+                .frame(width: 18)
+            Button {
+                print("order")
+            } label: {
+                Image("Order")
+                    .frame(width: 48, height: 48)
+                    .foregroundColor(Color("G3"))
+                    .background(Color("G1"))
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
+            }
+        }
+        .padding(.horizontal, 16.0)
+    }
 }
