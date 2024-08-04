4c5fcc09e9bfe85e0ca28e06f98ef0293ffcf56d
Sangmin Kim <noma@intereal.io>
2024-08-04 20:16:11 +0900

    add: add artist modal More page

--- Record/Views/Pages/More.swift
+++ Record/Views/Pages/More.swift
@@ -8,10 +8,71 @@
 import SwiftData
 import SwiftUI
 
+class ModalState: ObservableObject {
+    @Published var isAddArtistPresented: Bool = false
+}
+
+struct AddArtistModalView: View {
+    @ObservedObject var modalState: ModalState
+    @State var artistName: String = ""
+    @Environment(\.modelContext) private var modelContext
+
+    var body: some View {
+        VStack {
+            Spacer.vertical(16)
+            HStack {
+                Button(action: {
+                    self.modalState.isAddArtistPresented = false
+
+                }, label: {
+                    Text("Cancle")
+                        .font(Font.custom("Pretendard-Medium", size: 18))
+                        .foregroundStyle(Color("G5"))
+                })
+                Spacer()
+                Button(action: {
+                    let demoArtist = Artist(name: artistName, isGroup: false)
+                    modelContext.insert(demoArtist)
+                    do {
+                        try modelContext.save()
+                    } catch {
+                        NSLog("Failed to save")
+                    }
+                    self.modalState.isAddArtistPresented = false
+                    
+                }, label: {
+                    Text("Done")
+                        .font(Font.custom("Pretendard-Medium", size: 18))
+                        .foregroundStyle(Color("PointOrange"))
+                    
+                })
+            }
+            .padding(.horizontal, 16)
+            Spacer.vertical(24)
+            TextField("Enter name", text: $artistName)
+                .autocorrectionDisabled()
+                .submitLabel(.done)
+                .font(Font.custom("Pretendard-SemiBold", size: 18))
+                .foregroundStyle(Color("DefaultBlack"))
+                .padding(.horizontal, 16)
+                .padding(.vertical, 13.5)
+                .background(Color("G1"))
+                .clipShape(RoundedRectangle(cornerRadius: 4))
+                .padding(.horizontal, 16)
+                .onAppear {
+                    UITextField.appearance().clearButtonMode = .whileEditing
+                }
+            Spacer()
+        }
+    }
+}
+
 struct More: View {
     @EnvironmentObject var audioManager: AudioManager
     @Environment(\.modelContext) private var modelContext
     @Query var artists: [Artist]
+    
+    @StateObject var modalState = ModalState()
 
     var body: some View {
         VStack(spacing: 0) {
@@ -31,8 +92,10 @@ struct More: View {
                     MoreFeatureItem(icon: Image("music-solid"), text: "View All Tracks", onTabAction: {})
                 }
                 MoreFeatureGroup(sectionName: "Artists") {
-                    MoreFeatureItem(icon: Image("plus"), text: "Manage Artists", onTabAction: {})
-                    MoreFeatureItem(icon: Image("plus"), text: "Add Artist", onTabAction: {})
+                    MoreFeatureItem(icon: Image("user-solid"), text: "Manage Artists", onTabAction: {})
+                    MoreFeatureItem(icon: Image("plus"), text: "Add Artist", onTabAction: {
+                        self.modalState.isAddArtistPresented = true
+                    })
                 }
                 Spacer.vertical(24)
                 MoreFeatureGroup(sectionName: "Developer Only") {
@@ -202,6 +265,9 @@ struct More: View {
                 }
             }
         }
+        .sheet(isPresented: $modalState.isAddArtistPresented, content: {
+            AddArtistModalView(modalState: self.modalState)
+        })
     }
 }
 
